7b3c913ae54cb0b46c99629a93d56f036174ef57
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-06-08 14:09:25 +0300

    Add random id

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,11 +3,20 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class DemoController {
+
+    private static int ID = new Random().nextInt(10 - 1 + 1) + 1;
 
     @GetMapping("/evening")
     public String goodEvening() {
         return "Good evening, Sir!";
+    }
+
+    @GetMapping("/id")
+    public int id() {
+        return ID;
     }
 }
